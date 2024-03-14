91a6ef7452430b7659102a253d4b39c3501cbe9d
Deveshwar Saste <83347492+Deveshwar-Saste-9001@users.noreply.github.com>
2024-03-14 13:09:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
 
-const port = 3000;
+const port =  process.env.PORT || 3000;
 app.use(cors());
 var i=false;
 
